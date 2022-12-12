be193bf78476be01047ee9b8f36b73f6e1dbac1d
Doplax <doplax@gmail.com>
2022-12-13 00:00:49 +0100

    instalando Next.js

---
+++ blog-jamstack/lib/mdx.js
@@ -0,0 +1,46 @@
+import fs from "fs"// Vine instalado por defecto
+import path from "path";
+import matter from "gray-matter";
+import { serialze } from 'next-mdx-remote/serialize'
+
+//Ruta raiz
+const root = process.cwd();
+
+
+export const getFiles = () => {
+    // La funcion es Sincrona pq se van a llamar en tiempo de building
+    fs.readdirSync(path.join(root, "data"))
+};
+
+//Nos devolverá un unico fichero por la ruta, utilizara el slug (o endpoint) para buscar el fichero por su id
+export const getFileBySlug = async ({slug}) => {
+    const mdxSource = fs.readFileSync(path.js(root, "data", `${slug}.mdx}`), "utf-8");
+
+    //Data seran los "metadatos" y content el contenido del articulo
+    const {data,content} = await matter(mdxSource);
+
+    // podriamos importar un plugin para resaltar el codigo que se llama "markdown prism"
+    const source = await serialze(content, {})
+
+    return {
+        source,
+        frontmatter: {
+        slug,
+        ...data}
+    }
+};
+
+export const getAllFilesMetadata = () => {
+
+    //const files = fs.readdirSync(path.join(root,"data")); // Podemos llamr los ficheros directamente
+    const files = getFiles();
+
+    return files.reduce((allPosts, postSlug) => {
+        const mdxSource = fs.readFileSync(path.join(root, "data", postSlug), "utf-8")
+        const {data} = matter(mdxSource);
+
+        return[{...data, slug:postSlug.replace(".mdx","")}, ...allPosts]
+    },[]);
+};
+
+
