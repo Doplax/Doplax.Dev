7f9e78bc79f7043d825815981204061c83eb2009
Doplax <doplax@gmail.com>
2023-04-21 09:45:23 +0200

    Desactivando página de prubas para hacer deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+//  reactStrictMode: true,
+//}
+
+//module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    reactRefresh: true,
+  },
 }
 
 module.exports = nextConfig
