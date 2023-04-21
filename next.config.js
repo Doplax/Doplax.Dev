525cdd41e6d1366a01d3bef4e3540f24a741986e
Doplax <doplax@gmail.com>
2023-04-21 09:59:12 +0200

    Cambios en next.config

--- next.config.js
+++ next.config.js
@@ -8,9 +8,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    reactRefresh: true,
-  },
+  //experimental: {
+  //  reactRefresh: true,
+  //},
 }
 
 module.exports = nextConfig
