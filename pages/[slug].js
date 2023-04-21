1f169635b61c0b99ee3796d4d2e2963aebf23127
Doplax <doplax@gmail.com>
2023-04-21 09:14:06 +0200

    Refactorizazión y organización de los documentos

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,38 +1,35 @@
-// Los corchetes le estan diciendo a NEXT 
-//que lo de dentro es un parametro dimamico, 
-// y el slug sera lo que cambiará
-import {MDXRemote} from "next-mdx-remote" // Transforma el source que nos llega al leer el fichero en componentes (jsx en este caso) para que rencerice el mark down a el HTML final 
-import { getFileBySlug, getFiles } from "../lib/mdx"
-    import MDXComponents from '../components/MDXComponents.js'
+import { MDXRemote } from "next-mdx-remote"; // Importa el componente MDXRemote de la librería next-mdx-remote que se utiliza para renderizar código Markdown como componentes JSX
+import { getFileBySlug, getFiles } from "../lib/mdx"; // Importa las funciones getFileBySlug y getFiles del archivo mdx.js de la carpeta lib
+import MDXComponents from "../components/MDXComponents.js"; // Importa los componentes personalizados que se utilizan en los archivos Markdown
 
-export default function Post({ source, frontmatter}) {
-    return <MDXRemote {...source} components={MDXComponents}/>
+// Define el componente Post que se utiliza para mostrar los archivos Markdown
+export default function Post({ source, frontmatter }) {
+  return <MDXRemote {...source} components={MDXComponents} />;
 }
 
-// Saca el contenido y la data
-// Params es el slug que esta como parametro en el nombre del fichero
-export async function getStaticProps({params}){
-    //esta funcion esta definida en mdx.js
-    const {source, frontmatter} = await getFileBySlug(params.slug)
+// Obtiene el contenido y los metadatos del archivo Markdown
+// Params es el slug que está como parámetro en el nombre del fichero
+export async function getStaticProps({ params }) {
+  // Llama a la función getFileBySlug para obtener el contenido y los metadatos del archivo Markdown
+  const { source, frontmatter } = await getFileBySlug(params.slug);
 
-    return {
-        props: {source, frontmatter},
-    };
+  return {
+    props: { source, frontmatter },
+  };
 }
 
-// 
+// Obtiene los slugs de los archivos Markdown y los utiliza para crear los paths de las rutas
 export async function getStaticPaths() {
-    const posts = await getFiles();
-    const paths = posts.map((post) => ({
-        params: {
-            slug: post.replace(/\.mdx/, ""),
-        },
-    }))
+  const posts = await getFiles(); // Llama a la función getFiles para obtener los nombres de los archivos Markdown
+  const paths = posts.map((post) => ({
+    // Crea un objeto para cada archivo con el slug obtenido a partir del nombre del archivo
+    params: {
+      slug: post.replace(/\.mdx/, ""), // Remueve la extensión .mdx del nombre del archivo
+    },
+  }));
 
-    return{
-        paths,
-        // Si lo ponemos en false, cuando no encuentre la ruat dará un error 404
-        fallback: false
-    }
+  return {
+    paths,
+    fallback: false, // Si fallback es false, cuando no encuentre la ruta dará un error 404
+  };
 }
-
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import { getAllFilesMetadata } from '../lib/mdx'
+import { Footer } from '../components/Footer/Footer'
 
 
 // Recibos los post por props
@@ -33,18 +34,7 @@ export default function Home({posts}) {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer/>
     </div>
   )
 }
