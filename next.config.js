1e8b6df0e41b78671999d0bedb2fa50207572576
Doplax <doplax@gmail.com>
2023-04-25 08:03:22 +0200

    Antes del pull

---
+++ next.config.js
@@ -0,0 +1,27 @@
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+//  reactStrictMode: true,
+
+//}
+
+//module.exports = nextConfig
+
+// Import the necessary dependencies
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
+}
+
+// Merge MDX config with Next.js config
+module.exports = withMDX(nextConfig)
