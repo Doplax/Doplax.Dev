2825c8177f309ff4ce5f228f116f517d259bbc77
Doplax <doplax@gmail.com>
2023-05-08 10:30:03 +0200

    build: Contruido e implementado el componente Formulario (falta rematar el css)

--- components/Formulario/Formulario.jsx
+++ components/Formulario/Formulario.jsx
@@ -1,32 +1,33 @@
 import React from "react";
-import { render } from "react-dom";
-import './Formulario.css'
+import styles from './Formulario.module.css';
 
 function Formulario() {
     return (
-        <div>
-            <h2 className="section__title">CONTACTO</h2>
-            <div className="section__container">
-                <div className="contact-form">
-                    <form action="enviar.php" method="post" className="contact-form__form">
-                        <label htmlFor="fname" className="contact-form__label">Nombre y Apellidos</label>
-                        <input type="text" id="fname" name="nombre" placeholder="Introduzca su Nombre y Apellidos" required className="contact-form__input" />
-                        <label htmlFor="lname" className="contact-form__label">Email</label>
-                        <input type="text" id="lname" name="correo" placeholder="correo@ejemplo.com" required className="contact-form__input" />
-                        <label htmlFor="asunto" className="contact-form__label">Asunto</label>
-                        <input type="text" id="asunto" name="asunto" placeholder="Introduzca el Asunto del Mensaje" required className="contact-form__input" />
-                        <label htmlFor="subject" className="contact-form__label">Mensaje</label>
-                        <textarea id="subject" name="mensaje" placeholder="Escriba algo..." required className="contact-form__textarea"></textarea>
-                        <div className="privacy-policy">
-                            <input type="checkbox" id="privacy" name="privacy" value="Accepted" required className="privacy-policy__checkbox" />
-                            <label htmlFor="privacy" className="privacy-policy__label">
-                                <a className="privacy-policy__link" href="politica_de_privacidad.html" target="_blank">He leído y acepto la Política de Privacidad*</a>
-                            </label>
-                        </div>
-                        <div className="g-recaptcha" data-sitekey="6LdD3UocAAAAAJoT9GWU9TvRcwpTT7CUbn8_vXBK"></div>
-                        <button id="boton" type="submit" className="contact-form__button">ENVIAR</button>
-                    </form>
-                </div>
+        <div className={styles.form}>
+            <h2 className={styles.form__title}>CONTACTO</h2>
+            <div className={styles.form__container}>
+                <form action="enviar.php" method="post" className={styles.form__fields}>
+                    <label htmlFor="fname" className={styles.form__label}>Nombre y Apellidos</label>
+                    <input type="text" id="fname" name="nombre" placeholder="Introduzca su Nombre y Apellidos" required className={styles.form__input} />
+                    
+                    <label htmlFor="lname" className={styles.form__label}>Email</label>
+                    <input type="text" id="lname" name="correo" placeholder="correo@ejemplo.com" required className={styles.form__input} />
+                    
+                    <label htmlFor="asunto" className={styles.form__label}>Asunto</label>
+                    <input type="text" id="asunto" name="asunto" placeholder="Introduzca el Asunto del Mensaje" required className={styles.form__input} />
+                    
+                    <label htmlFor="subject" className={styles.form__label}>Mensaje</label>
+                    <textarea id="subject" name="mensaje" placeholder="Escriba algo..." required className={styles.form__textarea}></textarea>
+                    
+                    <div className={styles.form__privacy}>
+                        <input type="checkbox" id="privacy" name="privacy" value="Accepted" required className={styles.form__checkbox} />
+                        <label htmlFor="privacy" className={styles.form__privacyLabel}>
+                            <a className={styles.form__privacyLink} href="politica_de_privacidad.html" target="_blank">He leído y acepto la Política de Privacidad*</a>
+                        </label>
+                    </div>
+                    <div className="g-recaptcha" data-sitekey="6LdD3UocAAAAAJoT9GWU9TvRcwpTT7CUbn8_vXBK"></div>
+                    <button id="boton" type="submit" className={styles.form__button}>ENVIAR</button>
+                </form>
             </div>
         </div>
     );
--- pages/contacto.js
+++ pages/contacto.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import { Header } from '../components/Header/Header';
+import { Formulario } from '../components/Formulario/Formulario';
+
 import { Footer } from '../components/Footer/Footer';
 
 const Contacto = () => {
   return (
    <>
    <Header/>
-     <div className="mx-auto max-w-4xl py-12 px-4 sm:px-6 lg:px-8">
-      <h1 className="text-3xl font-bold mb-4">Contacto</h1>
-      <p className="text-lg mb-8">
-        ¿Tienes alguna pregunta o comentario sobre mis proyectos? ¡Contáctame usando el formulario de abajo!
-      </p>
-      <div className="flex justify-center">
-        <iframe
-          src="https://docs.google.com/forms/d/e/1FAIpQLSdSiAhhlDMLmciEZoc9BvrMgjaj0IUR6q4dxGlQEL14KDazKw/viewform?usp=sf_link"
-          width="100%"
-          height="600"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
-        >
-          Cargando…
-        </iframe>
-      </div>
-      <p className="text-lg mt-8">
-        O si lo prefieres, puedes contactarme directamente en:
-      </p>
-      <ul className="text-lg">
-        <li className="mb-2">Correo electrónico: ejemplo@ejemplo.com</li>
-        <li>Teléfono: 555-555-5555</li>
-      </ul>
+    <div className="mx-auto max-w-4xl py-12 px-4 sm:px-6 lg:px-8">
+      <Formulario/>  
     </div>
-    <Footer/>
+  <Footer/>
    </>
   );
 };
