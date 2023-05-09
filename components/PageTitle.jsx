4a546407599f83f4eae7b3773fcae693b026295e
Doplax <doplax@gmail.com>
2023-05-09 09:22:35 +0200

    build: 	- Creado componente <PageTitle> para usar el gradient como componente y poder usarlo en la página

---
+++ components/PageTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageTitle({ children }) {
+    return (
+        <h2 className='text-center text-6xl font-bold leading-tight tracking-tight'>
+            <span className="gradientText_animation">{children}</span>
+        </h2>
+    )
+}
+
+export { PageTitle }
