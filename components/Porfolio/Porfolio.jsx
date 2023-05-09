3bc1eedc9dd4c1546f99e2ab3b08d9519e5d7a1a
Doplax <doplax@gmail.com>
2023-05-09 12:58:21 +0200

    refactor: 	- Reorganización en el componente <Portfolio>

--- components/Porfolio/Porfolio.jsx
+++ components/Porfolio/Porfolio.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Porfolio.module.css";
 import { PageTitle } from "../PageTitle";
-import SkillSection from "../Porfolio/Skills/SkillSection";
-import Skill from "../Porfolio/Skills/Skill";
+import SkillSection from "./Skills/SkillSection";
+import Skill from "./Skills/Skill";
 
 const Portfolio = () => {
     const projects = [
@@ -157,11 +157,8 @@ const Portfolio = () => {
                     <div className={styles.home__description}></div>
                 </section>
 
-                <section id={styles.servicios__section} className={styles.section}>
-                    <PageTitle>SERVICIOS</PageTitle>
-                </section>
-            </main>
-            <div className="py-10">
+                {/* SOBRE MI */}
+                <div className="py-10">
                 <div>
                     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                         <PageTitle>Pol Valle</PageTitle>
@@ -174,8 +171,7 @@ const Portfolio = () => {
                         <div className="hidden md:flex items-center">
                             <div className="flex-col">
                                 <p className="text-lg  w-9/12 mb-5">
-                                    Lorem Ipsum es simplemente el texto de relleno de las
-                                    imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
+                                    Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de
                                     relleno estándar de las industrias desde el año 1500, cuando
                                     un impresor (N. del T. persona que se dedica a la imprenta)
                                     desconocido usó una galería de textos y los mezcló de tal
@@ -238,115 +234,156 @@ const Portfolio = () => {
                                     Linkedin
                                 </a>
                             </div>
-                        </div>
-
-                        {/* Proyectos */}
-                        <div className="px-4 py-8 sm:px-0">
-                            <h1 className="text-3xl font-bold mb-10">Proyectos</h1>
-                            <ul
-                                role="list"
-                                className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2"
-                            >
-                                {projects.map((project) => (
-                                    <li
-                                        key={project.name}
-                                        className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-slate-100 text-center shadow"
-                                    >
-                                        <div className="flex flex-1 flex-col p-8">
-                                            <img
-                                                className="mx-auto flex-shrink-0 max-h-56"
-                                                src={project.imageUrl}
-                                                alt=""
-                                            />
-                                            <h3 className="mt-6 mb-4 text-xl font-bold text-gray-900">
-                                                {project.name}
-                                            </h3>
-                                            <dl className="flex flex-grow flex-col justify-between">
-                                                <dd className="text-sm text-gray-500">
-                                                    {project.description}
-                                                </dd>
-                                                <dd className="mt-3">
-                                                    <span className="rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-800">
-                                                        {project.role}
-                                                    </span>
-                                                </dd>
-                                            </dl>
-                                        </div>
-                                        <div>
-                                            <div className="-mt-px flex divide-x divide-gray-200">
-                                                <div className="flex w-0 flex-1">
-                                                    <a
-                                                        href={project.github}
-                                                        target={"_blank"}
-                                                        className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                                                    >
-                                                        <img
-                                                            src="https://raw.githubusercontent.com/jmontes33/React-Portfolio/20d9cedab15245df7dbed1c63ebc814c6d655c57/src/assets/github-logo.svg"
-                                                            alt="github"
-                                                            className="h-5 w-5 text-gray-400"
-                                                            aria-hidden="true"
-                                                        />
-                                                        <span className="ml-3">Github</span>
-                                                    </a>
-                                                </div>
-                                                <div className="-ml-px flex w-0 flex-1">
-                                                    <a
-                                                        target={"_blank"}
-                                                        href={project.netlify}
-                                                        className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                                                    >
-                                                        {/*<ComputerDesktopIcon
-                            className="h-5 w-5 text-gray-400"
-                            aria-hidden="true"
-                          />*/}
-                                                        <span className="ml-3">Probar</span>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
+                        </div>   
                     </div>
                 </main>
-            </div>
-            <div className="mx-auto max-w-6xl sm:px-6 lg:px-8 py-10">
-                <PageTitle>Skills</PageTitle>
-                <SkillSection
-                    title="Front End"
-                    skills={frontEndSkills.map((skill) => (
-                        <Skill
-                            key={skill.alt}
-                            src={skill.src}
-                            alt={skill.alt}
-                            label={skill.label}
-                        />
-                    ))}
-                />
-                <SkillSection
-                    title="Back End"
-                    skills={backEndSkills.map((skill) => (
-                        <Skill
-                            key={skill.alt}
-                            src={skill.src}
-                            alt={skill.alt}
-                            label={skill.label}
-                        />
-                    ))}
-                />
-                <SkillSection
-                    title="Herramientas"
-                    skills={toolsSkills.map((skill) => (
-                        <Skill
-                            key={skill.alt}
-                            src={skill.src}
-                            alt={skill.alt}
-                            label={skill.label}
-                        />
-                    ))}
-                />
-            </div>
+                </div>
+
+                {/* PROYECTOS */}
+                <div className="px-4 py-8 sm:px-0">
+                                <h1 className="text-3xl font-bold mb-10">Proyectos</h1>
+                                <ul
+                                    role="list"
+                                    className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2"
+                                >
+                                    {projects.map((project) => (
+                                        <li
+                                            key={project.name}
+                                            className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-slate-100 text-center shadow"
+                                        >
+                                            <div className="flex flex-1 flex-col p-8">
+                                                <img
+                                                    className="mx-auto flex-shrink-0 max-h-56"
+                                                    src={project.imageUrl}
+                                                    alt=""
+                                                />
+                                                <h3 className="mt-6 mb-4 text-xl font-bold text-gray-900">
+                                                    {project.name}
+                                                </h3>
+                                                <dl className="flex flex-grow flex-col justify-between">
+                                                    <dd className="text-sm text-gray-500">
+                                                        {project.description}
+                                                    </dd>
+                                                    <dd className="mt-3">
+                                                        <span className="rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-800">
+                                                            {project.role}
+                                                        </span>
+                                                    </dd>
+                                                </dl>
+                                            </div>
+                                            <div>
+                                                <div className="-mt-px flex divide-x divide-gray-200">
+                                                    <div className="flex w-0 flex-1">
+                                                        <a
+                                                            href={project.github}
+                                                            target={"_blank"}
+                                                            className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                                                        >
+                                                            <img
+                                                                src="https://raw.githubusercontent.com/jmontes33/React-Portfolio/20d9cedab15245df7dbed1c63ebc814c6d655c57/src/assets/github-logo.svg"
+                                                                alt="github"
+                                                                className="h-5 w-5 text-gray-400"
+                                                                aria-hidden="true"
+                                                            />
+                                                            <span className="ml-3">Github</span>
+                                                        </a>
+                                                    </div>
+                                                    <div className="-ml-px flex w-0 flex-1">
+                                                        <a
+                                                            target={"_blank"}
+                                                            href={project.netlify}
+                                                            className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
+                                                        >
+                                                            {/*<ComputerDesktopIcon
+                                className="h-5 w-5 text-gray-400"
+                                aria-hidden="true"
+                            />*/}
+                                                            <span className="ml-3">Probar</span>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+
+                {/* SKILLS */}
+                <div className="mx-auto max-w-6xl sm:px-6 lg:px-8 py-10">
+                    <PageTitle>Skills</PageTitle>
+                    <SkillSection
+                        title="Front End"
+                        skills={frontEndSkills.map((skill) => (
+                            <Skill
+                                key={skill.alt}
+                                src={skill.src}
+                                alt={skill.alt}
+                                label={skill.label}
+                            />
+                        ))}
+                    />
+                    <SkillSection
+                        title="Back End"
+                        skills={backEndSkills.map((skill) => (
+                            <Skill
+                                key={skill.alt}
+                                src={skill.src}
+                                alt={skill.alt}
+                                label={skill.label}
+                            />
+                        ))}
+                    />
+                    <SkillSection
+                        title="Herramientas"
+                        skills={toolsSkills.map((skill) => (
+                            <Skill
+                                key={skill.alt}
+                                src={skill.src}
+                                alt={skill.alt}
+                                label={skill.label}
+                            />
+                        ))}
+                    />
+                </div>
+
+                {/* SERVICIOS */}
+                <section id={styles.servicios__section} className={styles.section}>
+                    <PageTitle>SERVICIOS</PageTitle>
+                    <div class="services__grid">
+                    <div class="services__item">
+                        <i class="fa-solid fa-mobile-screen-button"></i>
+                        <h3>Sitios Responsive</h3>
+                        <p>Su sitio se mostrará correctamente en cualquier dispositivo, computadoras, tabletas y teléfonos móviles.</p>
+                    </div>
+                    <div class="services__item">
+                        <i class="fa-solid fa-pen-ruler"></i>
+                        <h3>Diseños creativos</h3>
+                        <p>Un buen y atractivo diseño web lo ayuda a mantener potenciales clientes en su sitio, que es la cara digital de su negocio.</p>
+                    </div>
+                    <div class="services__item">
+                        <i class="fa-solid fa-code"></i>
+                        <h3>Desarrollo</h3>
+                        <p>Conozco la importancia del diseño web y puedo ayudarte a crear un sitio web que te encantará.</p>
+                    </div>
+                    <div class="services__item">
+                        <i class="fa-solid fa-thumbs-up"></i>
+                        <h3>Integración de redes sociales</h3>
+                        <p>Existen muchas plataformas sociales, y debe promover su presencia en el sitio web.</p>
+                    </div>
+                    <div class="services__item">
+                        <i class="fa-solid fa-gauge-high"></i>
+                        <h3>Rendimiento</h3>
+                        <p>Retener a los usuarios es crucial para un sitio web. Los sitios de alto rendimiento atraen y retienen a los usuarios.</p>
+                    </div>
+                    <div class="services__item">
+                        <i class="fa-solid fa-crosshairs"></i>
+                        <h3>SEO</h3>
+                        <p>Optimizaré su sitio con una estrategia inteligente de optimización de motores de búsqueda para generar clientes potenciales.</p>
+                    </div>
+                </div>
+                </section>
+
+            </main>
         </>
     );
 };
