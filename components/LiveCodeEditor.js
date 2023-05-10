af86e863d5cfe2c34798ce12376baf35fc9a159e
Doplax <doplax@gmail.com>
2023-05-10 12:27:36 +0200

    build: 	- Unificada la logica de los componentes anteriores, ahora todo se gestiona desde LiveCodeEditor

--- components/LiveCodeEditor.js
+++ components/LiveCodeEditor.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
-//import CodeBlock from '../components/CodeBlock';
+import CodeBlock from './CodeBlock';
+import PrismTheme from 'prism-react-renderer/themes/nightOwl';
 
-export default function LiveCodeBlock({ code }) {
+const LiveCodeEditor = ({ code, editable = false }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: '1rem', marginBottom: '1rem' }}>
-      <div style={{ marginRight: '1rem' }}>
-        <h3>Código editable:</h3>
-        <LiveProvider   LiveProvider code={code}  noInline>
-          <LiveEditor />
+    <LiveProvider code={code} theme={PrismTheme} noInline>
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', marginTop: '1rem', marginBottom: '1rem' }}>
+        <div style={{ marginRight: '1rem' }}>
+          {editable ? (
+            <>
+              <h3>Código editable:</h3>
+              <LiveEditor />
+            </>
+          ) : (
+            <>
+              <h3>Código:</h3>
+              <CodeBlock className="language-javascript">{code}</CodeBlock>
+            </>
+          )}
           <LiveError />
-        </LiveProvider>
-      </div>
-      <div>
-        <h3>Live Preview:</h3>
-        <LiveProvider code={code}  noInline>
+        </div>
+        <div>
+          <h3>Live Preview:</h3>
           <LivePreview />
-        </LiveProvider>
+        </div>
       </div>
-    </div>
+    </LiveProvider>
   );
-}
+};
+
+export { LiveCodeEditor };
