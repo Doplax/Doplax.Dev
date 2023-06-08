482b524ab964eb085dfcdca08b0f5bd254c5e744
Doplax <doplax@gmail.com>
2023-06-08 09:15:36 +0200

    fix: Quitando javascriptCopy code del componente codeblock

--- components/CodeBlock.jsx
+++ components/CodeBlock.jsx
@@ -1,31 +1,3 @@
-//// Este componente se encarga de renderizar el visór del bloques de código.
-
-//import React from 'react';
-//import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-
-//import { railscasts  } from 'react-syntax-highlighter/dist/cjs/styles/hljs'; // Puedes elegir otro tema si prefieres
-
-//const CodeBlock = ({ children, className }) => {
-//  const language = className ? className.replace(/language-/, '') : 'javascript';
-
-//  try {
-//    return (
-//      <SyntaxHighlighter language={language} style={ railscasts }>
-//        {children.trim()}
-//      </SyntaxHighlighter>
-//    );
-//  } catch (error) {
-//    console.error(`Error highlighting code with language "${language}": ${error}`);
-//    return (
-//      <pre>
-//        <code className={className}>{children.trim()}</code>
-//      </pre>
-//    );
-//  }
-//};
-
-//export default CodeBlock;
-
 import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { railscasts  } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
@@ -39,17 +11,19 @@ const CodeBlock = ({node, inline}) => {
   }
   
   const language = node.properties.className[0].replace(/language-/, '');
+  const code = node.children[0].value.trim().replace(/^javascriptCopy code\n/, ''); // Esto quitará "javascriptCopy code"
+  
   try {
     return (
       <SyntaxHighlighter language={language} style={railscasts}>
-        {node.children[0].value.trim()}
+        {code}
       </SyntaxHighlighter>
     );
   } catch (error) {
     console.error(`Error highlighting code with language "${language}": ${error}`);
     return (
       <pre>
-        <code className={node.properties.className}>{node.children[0].value.trim()}</code>
+        <code className={node.properties.className}>{code}</code>
       </pre>
     );
   }
