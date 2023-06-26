ec7a718af5a525579c84701f3c41cbc7268cf366
Doplax <doplax@gmail.com>
2023-06-26 11:37:06 +0200

    Arreglado el layout, ahora hay que ir añadiendo las páginas

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Layout } from '../../components/Layout';
 import { Formulario } from '../../components/Formulario/Formulario';
 
 const Contacto = () => {
   return (
-    <Layout>
+
       <Formulario/>        
-    </Layout>
+  
 
   );
 };
