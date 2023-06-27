a0282fe61ac04ecdcb9ac29b65a556dc4a1aeaa4
Doplax <doplax@gmail.com>
2023-06-27 18:44:17 +0200

    build: Implementación de ChatGPT

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,47 @@
+'use client'
+import { useChat } from 'ai/react'
+import './Chat.css' 
+
+function Chat() {
+    const { messages, input, handleInputChange, handleSubmit } = useChat({
+        api: '/api/chat'
+    })
+
+    return (
+        <div className='chat-container'>
+            <div className='chat-messages'>
+                {
+                    messages.map(message => {
+                        const isChat = message.role !== 'user'
+                        return (
+                            <div key={message.id} className={`message ${isChat ? 'chatbot' : 'user'}`}>
+                                <div className='message-bubble'>
+                                    {message.content}
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <form onSubmit={handleSubmit} className="chat-form">
+                <input 
+                    placeholder='Escribe al chat'
+                    type='text' 
+                    id="content"
+                    name='content' 
+                    value={input} 
+                    onChange={handleInputChange} 
+                    className="chat-input"
+                />
+                <button 
+                    type="submit" 
+                    className="chat-button"
+                >
+                    Enviar
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export { Chat };
