2e0794b099339ff1280d7ab686d88c96cc12b4a4
Doplax <doplax@gmail.com>
2023-06-28 11:37:37 +0200

    rebuild: Para cargar las imagenes estaticas de notiony

--- src/components/Porfolio/HomeSection/HomeSection.jsx
+++ src/components/Porfolio/HomeSection/HomeSection.jsx
@@ -63,7 +63,7 @@ function HomeSection() {
                                     type="button"
                                     className="inline-flex items-center rounded-md border border-gray-300 bg-white px-6 py-3 text-xl font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                 >
-                                    Descargar CV
+                                    Descargar CV 
                                 </a>
                             </div>
                         </div>
