f181f8fe0aade2fa6f976d508e8eea532d32db7a
Doplax <doplax@gmail.com>
2023-07-01 13:43:55 +0200

    Feat: 	- Creado el componente HambBtn

---
+++ src/app/pruebas/page.jsx
@@ -0,0 +1,12 @@
+//import { Modal } from '../../components/Modal/Modal'
+import { Pruebas } from '../../components/Pruebas/Pruebas'
+<Pruebas/>
+
+
+export default function pruebas() {
+
+  return (
+    <Pruebas/>
+  )
+}
+//export default Home;
