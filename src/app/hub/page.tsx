9d62ef59c1dc125f6c64178ddac584f11996405d
Doplax <doplax@gmail.com>
2024-04-25 14:09:51 +0200

    Chore: Remove fontAwesome and hub Page

--- src/app/hub/page.tsx
+++ src/app/hub/page.tsx
@@ -5,12 +5,12 @@ const Hub = () => {
         <div>
             <div className={styles.container}>
                 <ul>
-                    <li style={{ '--i': 6, '--clr': '#131212' }}><a href="https://github.com/Doplax"><span><i className="fa-brands fa-github"></i></span> Github</a></li>
+                    {/*<li style={{ '--i': 6, '--clr': '#131212' }}><a href="https://github.com/Doplax"><span><i className="fa-brands fa-github"></i></span> Github</a></li>
                     <li style={{ '--i': 5, '--clr': '#f2a918' }}><a href="https://codepen.io/doplax-the-scripter"><span><i className="fa-brands fa-codepen"></i></span>Components</a></li>
                     <li style={{ '--i': 4, '--clr': '#1da1f2' }}><a href="https://twitter.com/DoplaxDev"><span><i className="fa-brands fa-twitter"></i></span> Twitter</a></li>
                     <li style={{ '--i': 3, '--clr': '#c32aa3' }}><a href="https://www.instagram.com/doplax.dev/"><span><i className="fa-brands fa-instagram"></i></span> Instagram</a></li>
                     <li style={{ '--i': 2, '--clr': '#ff0000' }}><a href="https://www.youtube.com/channel/UCf0IJU9vXlcUd2CALEBA93A"><span><i className="fa-brands fa-youtube"></i></span> Youtube</a></li>
-                    <li style={{ '--i': 1, '--clr': '#0a66c2' }}><a href="https://www.linkedin.com/in/pol-valle-montes/"><span><i className="fa-brands fa-linkedin"></i></span> Linkedin</a></li>
+                    <li style={{ '--i': 1, '--clr': '#0a66c2' }}><a href="https://www.linkedin.com/in/pol-valle-montes/"><span><i className="fa-brands fa-linkedin"></i></span> Linkedin</a></li>*/}
                 </ul>
             </div>
         </div>
