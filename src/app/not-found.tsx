f89da17c9caa1df3db23fb91da2651016818f042
Pol Valle (Doplax) <doplax@gmail.com>
2024-07-03 09:57:07 +0200

    feat(not-found): improve 404 page styling and content
    Enhanced the 404 error page by adding imports for Button, Link, and PageTitle components. Updated the page structure with a flex layout, centered content, and a styled button that links back to the homepage.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import { PageTitle } from '@/components/PageTitle/PageTitle'
+
+// Also investigate error page:
+// https://nextjs.org/docs/app/building-your-application/routing/error-handling
+
+
 export default function NotFound() {
     return (
-        <>
-            <h1>Error not found </h1>
-        </>
-    )
+        <div className="flex flex-col justify-center items-center h-[80vh]">
+            <PageTitle> Error 404</PageTitle>
+            <h2>Page Not Found</h2>
+            <Link href="/" >
+                <Button color="warning"
+                    variant="flat"
+                    className="text-gray-100 text-lg font-bold p-8 mt-7">Back to Home</Button>
+            </Link >
+        </div>
+    );
 }
